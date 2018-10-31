24832492503b69397bce39679bd8c18e37c4bd7d
Cprocc <755516806@qq.com>
2018-10-31 22:38:50 +0800

    view的Get属性

--- booktest2/urls.py
+++ booktest2/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)', views.detail),
+    url(r'^link_page/$', views.link_page),
+    url(r'^one_key_one_value/$', views.one_key_one_value),
+    url(r'^one_key_more_value/$', views.one_key_more_value)
 ]
--- booktest2/views.py
+++ booktest2/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.db.models import Max, F, Q
 from .models import *
+from django.http import HttpResponse
+
 # Create your views here.
 
 
@@ -18,3 +20,28 @@ def index(request):
         'list': list_read_more_than_comment
     }
     return render(request, 'booktest2/index.html', context)
+
+
+# 返回一个带有请求连接"localhost:8000/booktest2/...."中参数的内容
+def detail(requset, year, month, day):
+    return HttpResponse("year:{} month:{} day:{}".format(year, month, day))
+
+
+def link_page(request):
+    return render(request, 'booktest2/link_page.html')
+
+
+def one_key_one_value(request):
+    # request.GET.get()
+    # 根据key接收值
+    a1 = request.GET['a']
+    b1 = request.GET['b']
+    c1 = request.GET['c']
+    context = {"a": a1, "b": b1, 'c': c1}
+    return render(request, 'booktest2/one_key_one_value.html', context)
+
+
+def one_key_more_value(request):
+    a1 = request.GET.getlist('a')
+    context = {"a": a1}
+    return render(request, 'booktest2/one_key_more_value.html', context)
